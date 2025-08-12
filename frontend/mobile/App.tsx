da79fed84d3edeeb5a32b03b0f20f35f65d03d2d
sampocs <sam.pochyly@gmail.com>
2025-08-12 14:34:10 -0500

    icons

--- frontend/mobile/App.tsx
+++ frontend/mobile/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import PortfolioScreen from './src/screens/PortfolioScreen';
 import AllocationsScreen from './src/screens/AllocationsScreen';
@@ -16,17 +16,17 @@ export default function App() {
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
-            let iconName: keyof typeof Ionicons.glyphMap;
+            let iconName: string;
 
             if (route.name === 'Portfolio') {
-              iconName = focused ? 'trending-up' : 'trending-up-outline';
+              iconName = 'chart-area';
             } else if (route.name === 'Allocations') {
-              iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+              iconName = 'chart-pie';
             } else {
-              iconName = 'help-outline';
+              iconName = 'question';
             }
 
-            return <Ionicons name={iconName} size={size} color={color} />;
+            return <FontAwesome5 name={iconName} size={size} color={color} solid={focused} />;
           },
           tabBarActiveTintColor: theme.colors.foreground,
           tabBarInactiveTintColor: theme.colors.muted,
