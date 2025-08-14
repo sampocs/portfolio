972836a1b053d2dc887f522885d2640a8747ad50
sampocs <sam.pochyly@gmail.com>
2025-08-14 17:28:07 -0500

    increased db pooling in api

--- backend/backend/database/connection.py
+++ backend/backend/database/connection.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from backend.config import config
 
-engine = create_engine(config.postgres_url)
+engine = create_engine(config.postgres_url, pool_size=15, max_overflow=10, pool_timeout=60)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
